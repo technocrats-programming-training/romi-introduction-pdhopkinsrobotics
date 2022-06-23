cf89b8b96c7f30ba6276763311323eeb8c34d84f
Paul Dirkswager <107653146+pdhopkinsrobotics@users.noreply.github.com>
2022-06-23 23:30:27 +0000

    Test romi 1

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,22 @@ public class Robot extends ProceduralRobot {
   @Override
   public void autonomousProcedure() throws InterruptedException {
     // Write your code here:
+    int rightSpeed = 1;
+    int rightSeconds90 = 1;
+    int leftSpeed = -1;
+    int leftSeconds90 = 1;
 
+    
+    driveTime(1, 2); //drive up
+    turnTime(rightSpeed, rightSeconds); //90 right
+    driveTime(1, 1); //drive right
+    turnTime(rightSpeed, rightSeconds); //90 right
+    driveTime(1, 1); //drive down
+    turnTime(rightSpeed, rightSeconds); //90 right
+    driveTime(1, 1); //drive left
+    turnTime(leftSpeed, leftSeconds); //90 left
+    driveTime(1, 1); //drive down
+    sleepSeconds(2);
+    driveTime(1, .5); //drive down
   }
 }
